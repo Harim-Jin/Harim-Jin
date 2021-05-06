80e0b69cabb52354efacbbb3f6c1c3637f4bc8d7
Harim-Jin <83169442+Harim-Jin@users.noreply.github.com>
2021-05-06 14:55:41 +0900

    Update xrpauto.py

--- xrpauto.py
+++ xrpauto.py
@@ -57,7 +57,6 @@ print("autotrade start")
 # 시작 메세지 슬랙 전송
 post_message((myToken,"#jcoin", "리플 자동매매 시작!")
 
-
 while True:
     try:
         now = datetime.datetime.now()
@@ -69,18 +68,19 @@ while True:
             ma15 = get_ma15("KRW-XRP")
             current_price = get_current_price("KRW-XRP")
             if target_price < current_price and ma15 < current_price:
-                krw = get_balance("KRW")
+                krw = get_balance("XRP")
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-XRP", (krw*0.9995)*0.25)
-                    post_message(myToken,"#jcoin", "XRP buy : " +str(buy_result))
+                    buy_result = upbit.buy_market_order("KRW-XRP", ( krw*0.9995)*0.25)
+                    post_message(myToken,"#bit", "XRP buy : " +str(buy_result))
         else:
-            eth = get_balance("XRP")
-            current_price = get_current_price("KRW-XRP")
-            avg = upbit.upbit.avg_buy_price("KWR-XRP")
-            if ETH > 0.002 or avg + (avg*0.2) < current_price:
-                sell_result = upbit.sell_market_order("KRW-XRP", eth*0.9995)
-                post_message(myToken,"#jcoin", "XRP sell : " +str(sell_result))
-        time.sleep(1)
+            xrp = get_balance("XRP")
+            currnt_price = get_current_price("KWR-XRP")
+            avg = upbit.avg_buy_price("KWR-XRP")
+                if xrp > 0.000073 or avg + (avg*0.2) < current_price:
+                    sell_result = upbit.sell_market_order("KRW-XRP", btc*0.9995)
+                    post_message(myToken,"#bit", "XRP sell : " +str(sell_result))
+        time.sleep(1)             
+
     except Exception as e:
         print(e)
         post_message(myToken,"#jcoin", "XRP Error")
