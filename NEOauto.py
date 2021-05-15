a495f9ae3ebe173993fb7b692669deddecc03195
Harim-Jin <83169442+Harim-Jin@users.noreply.github.com>
2021-05-15 18:34:35 +0900

    Update NEOauto.py

--- NEOauto.py
+++ NEOauto.py
@@ -67,7 +67,7 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-NEO", 0.5)
+            target_price = get_target_price("KRW-NEO", 0.4)
             ma15 = get_ma15("KRW-NEO")
             current_price = get_current_price("KRW-NEO")
             if target_price < current_price and ma15 < current_price:
